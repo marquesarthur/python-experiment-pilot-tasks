4cf0a18e8dd7bc82e1a9af7981331061d691b81a
marquesarthur <marques.s.arthur@gmail.com>
2021-11-23 16:15:51 -0800

    adding artifacts for task 4

--- tasks/resumes.py
+++ tasks/resumes.py
@@ -1,7 +1,13 @@
-
+# https://pymotw.com/3/re/index.html
+# https://docs.python.org/3/tutorial/inputoutput.html
+# https://docs.python.org/3/library/re.html
+# https://www.pythontutorial.net/python-basics/python-read-text-file/
+# https://www.c-sharpcorner.com/article/how-to-validate-an-email-address-in-python/
+# https://stackoverflow.com/questions/42407785/regex-extract-email-from-strings
 # https://stackoverflow.com/questions/28840903/python-regex-match-middle-of-string
 # https://stackoverflow.com/questions/180986/what-is-the-difference-between-re-search-and-re-match?noredirect=1&lq=1
-
+# https://stackoverflow.com/questions/8022530/how-to-check-for-valid-email-address
+# https://stackoverflow.com/questions/4666973/how-to-extract-the-substring-between-two-markers
 
 import re
 
